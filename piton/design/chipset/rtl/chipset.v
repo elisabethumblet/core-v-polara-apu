5d50d096c87bcb4cefcd2f59061ca26d11ce147e
elisabethumblet <humblet@pcys25.grm.polymtl.ca>
2023-08-11 12:13:35 -0400

    Add Alveo U280 config options and IPs

--- piton/design/chipset/rtl/chipset.v
+++ piton/design/chipset/rtl/chipset.v
@@ -88,7 +88,16 @@ module chipset(
 
 `ifdef F1_BOARD
     input sys_clk,
-`else
+`elsif ALVEO_BOARD
+    input         pcie_refclk_clk_n    ,
+    input         pcie_refclk_clk_p    ,
+    input         pcie_perstn          ,		
+    input  [15:0] pci_express_x16_rxn  ,
+    input  [15:0] pci_express_x16_rxp  ,
+    output [15:0] pci_express_x16_txn  ,
+    output [15:0] pci_express_x16_txp  ,
+    input         resetn ,
+    output        chip_rstn ,
     // Oscillator clock
 `ifdef PITON_CHIPSET_CLKS_GEN
     `ifdef PITON_CHIPSET_DIFF_CLK
@@ -244,11 +253,11 @@ module chipset(
     output [`DDR3_CS_WIDTH-1:0]                 ddr_cs_n,
 `endif // endif NEXYSVIDEO_BOARD
 `ifdef PITONSYS_DDR4
-`ifdef XUPP3R_BOARD
+`ifdef XUPP3R_OR_ALVEO
     output                                      ddr_parity,
 `else
     inout [`DDR3_DM_WIDTH-1:0]                  ddr_dm,
-`endif // XUPP3R_BOARD
+`endif // XUPP3R_OR_ALVEO
 `else // PITONSYS_DDR4
     output [`DDR3_DM_WIDTH-1:0]                 ddr_dm,
 `endif // PITONSYS_DDR4
@@ -462,13 +471,13 @@ module chipset(
     `ifdef VCU118_BOARD
         // we only have 4 gpio dip switches on this board
         input  [3:0]                                        sw,
-    `elsif XUPP3R_BOARD
+    `elsif XUPP3R_OR_ALVEO
         // no switches :(
     `else         
         input  [7:0]                                        sw,
     `endif
 
-    `ifdef XUPP3R_BOARD
+    `ifdef XUPP3R_OR_ALVEO
      output [3:0]                                           leds
     `else 
      output [7:0]                                           leds
@@ -755,7 +764,7 @@ end
             `ifdef VCU118_BOARD
                 assign uart_boot_en    = sw[0];
                 assign uart_timeout_en = sw[1];
-            `elsif XUPP3R_BOARD
+            `elsif XUPP3R_OR_ALVEO
                 assign uart_boot_en    = 1'b1;
                 assign uart_timeout_en = 1'b0;
             `else 
@@ -810,6 +819,11 @@ end
     assign tp[7:0] = 8'd0;
 `elsif XUPP3R_BOARD
     assign leds[0] = ~piton_ready_n;
+    assign leds[1] = init_calib_complete;
+    assign leds[2] = processor_offchip_noc2_valid;
+    assign leds[3] = offchip_processor_noc3_valid;
+`elsif ALVEO_BOARD
+    assign leds[0] = 1'b1;
     assign leds[1] = init_calib_complete;
     assign leds[2] = processor_offchip_noc2_valid;
     assign leds[3] = offchip_processor_noc3_valid;
@@ -1307,11 +1321,11 @@ chipset_impl_noc_power_test  chipset_impl (
                     .ddr_cs_n(ddr_cs_n),
                 `endif // endif NEXYSVIDEO_BOARD
             
-                `ifdef XUPP3R_BOARD
+                `ifdef XUPP3R_OR_ALVEO
                     .ddr_parity(ddr_parity),
                 `else
                     .ddr_dm(ddr_dm),
-                `endif // XUPP3R_BOARD
+                `endif // XUPP3R_OR_ALVEO
                 .ddr_odt(ddr_odt)
             `else // ifndef F1_BOARD
                 .mc_clk(mc_clk),
@@ -1420,6 +1434,20 @@ chipset_impl_noc_power_test  chipset_impl (
 
             `endif // PITON_FPGA_ETHERNETLITE   
     `endif // endif PITONSYS_IOCTRL
+
+    `ifdef ALVEO_BOARD
+        ,    // PCIe
+        .pci_express_x16_rxn(pci_express_x16_rxn),
+        .pci_express_x16_rxp(pci_express_x16_rxp),
+        .pci_express_x16_txn(pci_express_x16_txn),
+        .pci_express_x16_txp(pci_express_x16_txp),
+        .pcie_perstn(pcie_perstn),
+        .pcie_refclk_clk_n(pcie_refclk_clk_n),
+        .pcie_refclk_clk_p(pcie_refclk_clk_p),
+        .resetn(resetn),
+        .chip_rstn (chip_rstn)
+    
+    `endif
 
     `ifdef PITON_RV64_PLATFORM
     `ifdef PITON_RV64_DEBUGUNIT
--- piton/tools/src/proto/fpga_lib.py
+++ piton/tools/src/proto/fpga_lib.py
@@ -48,6 +48,7 @@ MAP_MODULE_NAME = "storage_addr_trans.v"
 NOC_PAYLOAD_WIDTH = 512
 STORAGE_BLOCK_BIT_WIDTH         =   {   "ddr":  {   "vc707":512,
                                                     "vcu118":512,
+                                                    "alveou280":512,
                                                     "xupp3r":512,
                                                     "nexys4ddr":128,
                                                     "genesys2":256,
@@ -56,6 +57,7 @@ STORAGE_BLOCK_BIT_WIDTH         =   {   "ddr":  {   "vc707":512,
                                                 },
                                         "bram": {   "vc707":512,
                                                     "vcu118":512,
+                                                    "alveou280":512,
                                                     "xupp3r":512,
                                                     "nexys4ddr":512,
                                                     "genesys2":512,
@@ -65,6 +67,7 @@ STORAGE_BLOCK_BIT_WIDTH         =   {   "ddr":  {   "vc707":512,
                                                 },
                                         "dmw":  {   "vc707":512,
                                                     "vcu118":512,
+                                                    "alveou280":512,
                                                     "xupp3r":512,
                                                     "nexys4ddr":512,
                                                     "genesys2":512,
@@ -76,6 +79,7 @@ STORAGE_BLOCK_BIT_WIDTH         =   {   "ddr":  {   "vc707":512,
 
 STORAGE_ADDRESSABLE_BIT_WIDTH   =   {   "ddr":  {   "vc707":64,
                                                     "vcu118":64,
+                                                    "alveou280":72,
                                                     "xupp3r":64,
                                                     "nexys4ddr":16,
                                                     "genesys2":32,
@@ -84,6 +88,7 @@ STORAGE_ADDRESSABLE_BIT_WIDTH   =   {   "ddr":  {   "vc707":64,
                                                 },
                                         "bram": {   "vc707":512,
                                                     "vcu118":512,
+                                                    "alveou280":512,
                                                     "xupp3r":512,
                                                     "nexys4ddr":512,
                                                     "genesys2":512,
@@ -93,17 +98,21 @@ STORAGE_ADDRESSABLE_BIT_WIDTH   =   {   "ddr":  {   "vc707":64,
                                                 },
                                         "dmw": {    "vc707":512,
                                                     "vcu118":512,
+                                                    "alveou280":512,
                                                     "xupp3r":512,
                                                     "nexys4ddr":512,
                                                     "genesys2":512,
                                                     "nexysVideo":512,
                                                     "piton_board":512,
                                                     "f1":512
+                                                },
+                                        "hbm": {    "alveou280":33
                                                 }
                                     }
 
 STORAGE_BIT_SIZE                =   {   "ddr":  {   "vc707":8*2**30,
                                                     "vcu118":2*8*2**30,
+                                                    "alveou280":2*8*2**30,
                                                     "xupp3r":32*8*2**30,
                                                     "nexys4ddr":8*128*2**20,
                                                     "genesys2":8*2**30,
@@ -112,6 +121,7 @@ STORAGE_BIT_SIZE                =   {   "ddr":  {   "vc707":8*2**30,
                                                 },
                                         "bram": {   "vc707":16384*512,
                                                     "vcu118":16384*512,
+                                                    "alveou280":116384*512,
                                                     "xupp3r":16384*512,
                                                     "nexys4ddr":16384*512,
                                                     "genesys2":16384*512,
@@ -121,11 +131,14 @@ STORAGE_BIT_SIZE                =   {   "ddr":  {   "vc707":8*2**30,
                                                 },
                                         "dmw":  {   "vc707":8*2**30,
                                                     "vcu118":2*8*2**30,
+                                                    "alveou280":2*8*2**30,
                                                     "xupp3r":32*8*2**30,
                                                     "nexys4ddr":8*128*2**20,
                                                     "genesys2":8*2**30,
                                                     "nexysVideo":8*512*2**20,
                                                     "f1":8*4*2**30
+                                                },
+                                        "hbm":  {   "alveou280":8*4*2*33
                                                 }
                                     }
 DW_BIT_SIZE     = 64
@@ -345,7 +358,7 @@ def buildProjectSuccess(log_dir):
         dbg.print_error("Check: %s" % fpath)
         return False
 
-    dbg.print_info("Project was build successfully!")
+    dbg.print_info("Project was built successfully!")
     return True
 
 
