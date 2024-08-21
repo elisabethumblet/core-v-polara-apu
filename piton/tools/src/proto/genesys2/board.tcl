5d25839f3d811ea26c8f0adf0ee3e2d86f39706b
rrpsid <rrpsid@gmail.com>
2024-08-21 18:09:48 -0400

    Modified files to include loopback module in flow. --poloopback option to test Polara in loopback.

--- piton/tools/src/proto/genesys2/board.tcl
+++ piton/tools/src/proto/genesys2/board.tcl
@@ -33,8 +33,17 @@ set FPGA_PART "xc7k325tffg900-2"
 set VIVADO_FLOW_PERF_OPT 0
 set BOARD_DEFAULT_VERILOG_MACROS "GENESYS2_BOARD"
 
-# Single ended clock (for MIG input clock) has a specific block design
-if { [ info exists ::env(POLARA_GEN2_CHIPSETSE) ] } {
+# First check if we are doing loopback test
+if { [ info exists ::env(POLARA_LOOPBACK) ] } {
+    # Create a block design containing a JTAG-AXI master using the FPGA_PART variable
+    # It will produce the "gen2_polara_fpga.bd" file
+
+    source $DV_ROOT/tools/src/proto/${BOARD}/gen2_polara_fpga_loopback.tcl
+
+    # Grab the file from where the above tcl script has placed it
+    set DESIGN_BD_FILES [list $DV_ROOT/design/chipset/xilinx/genesys2/gen2_polara_fpga_loopback/gen2_polara_fpga_loopback]
+} elseif { [ info exists ::env(POLARA_GEN2_CHIPSETSE) ] } {
+    # Single ended clock (for MIG input clock) has a specific block design
     # Create a block design containing a JTAG-AXI master using the FPGA_PART variable
     # It will produce the "gen2_polara_fpga.bd" file
 
--- piton/tools/src/proto/genesys2/gen2_polara_fpga_loopback.tcl
+++ piton/tools/src/proto/genesys2/gen2_polara_fpga_loopback.tcl
@@ -18,19 +18,6 @@ variable script_folder
 set script_folder [_tcl::get_script_folder]
 
 ################################################################
-# Check if script is running in correct Vivado version.
-################################################################
-set scripts_vivado_version 2021.1
-set current_vivado_version [version -short]
-
-if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
-   puts ""
-   catch {common::send_gid_msg -ssname BD::TCL -id 2041 -severity "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
-
-   return 1
-}
-
-################################################################
 # START
 ################################################################
 
@@ -41,16 +28,25 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 # project, but make sure you do not have an existing project
 # <./myproj/project_1.xpr> in the current working folder.
 
+set DV_ROOT $::env(DV_ROOT)
+set PITON_ROOT $::env(PITON_ROOT)
+
+set tmp_build_dir ${PITON_ROOT}/build/genesys2/bd_gen2
+set tmp_prj "create_bd"
+
+file delete -force ${tmp_build_dir}/${tmp_prj}
+
 set list_projs [get_projects -quiet]
 if { $list_projs eq "" } {
-   create_project project_1 myproj -part xc7k325tffg900-2
+    create_project -force ${tmp_build_dir}/${tmp_prj} -part xc7k325tffg900-2    
+    #   create_project project_1 myproj -part xc7k325tffg900-2
    set_property BOARD_PART digilentinc.com:genesys2:part0:1.1 [current_project]
 }
 
 
 # CHANGE DESIGN NAME HERE
 variable design_name
-set design_name gen2_polara_fpga
+set design_name gen2_polara_fpga_loopback
 
 # If you do not already have an existing IP Integrator design open,
 # you can create a design using the following command:
@@ -63,94 +59,10 @@ set nRet 0
 set cur_design [current_bd_design -quiet]
 set list_cells [get_bd_cells -quiet]
 
-if { ${design_name} eq "" } {
-   # USE CASES:
-   #    1) Design_name not set
-
-   set errMsg "Please set the variable <design_name> to a non-empty value."
-   set nRet 1
-
-} elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
-   # USE CASES:
-   #    2): Current design opened AND is empty AND names same.
-   #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
-   #    4): Current design opened AND is empty AND names diff; design_name exists in project.
-
-   if { $cur_design ne $design_name } {
-      common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
-      set design_name [get_property NAME $cur_design]
-   }
-   common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Constructing design in IPI design <$cur_design>..."
-
-} elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
-   # USE CASES:
-   #    5) Current design opened AND has components AND same names.
-
-   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
-   set nRet 1
-} elseif { [get_files -quiet ${design_name}.bd] ne "" } {
-   # USE CASES: 
-   #    6) Current opened design, has components, but diff names, design_name exists in project.
-   #    7) No opened design, design_name exists in project.
-
-   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
-   set nRet 2
-
-} else {
-   # USE CASES:
-   #    8) No opened design, design_name not in project.
-   #    9) Current opened design, has components, but diff names, design_name not in project.
-
-   common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Currently there is no design <$design_name> in project, so creating one..."
-
-   create_bd_design $design_name
-
-   common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Making design <$design_name> as current_bd_design."
-   current_bd_design $design_name
-
-}
+create_bd_design $design_name -dir $DV_ROOT/design/chipset/xilinx/genesys2
+current_bd_design $design_name
 
 common::send_gid_msg -ssname BD::TCL -id 2005 -severity "INFO" "Currently the variable <design_name> is equal to \"$design_name\"."
-
-if { $nRet != 0 } {
-   catch {common::send_gid_msg -ssname BD::TCL -id 2006 -severity "ERROR" $errMsg}
-   return $nRet
-}
-
-set bCheckIPsPassed 1
-##################################################################
-# CHECK IPs
-##################################################################
-set bCheckIPs 1
-if { $bCheckIPs == 1 } {
-   set list_check_ips "\ 
-xilinx.com:ip:axi_gpio:2.0\
-xilinx.com:ip:jtag_axi:1.2\
-xilinx.com:ip:proc_sys_reset:5.0\
-xilinx.com:ip:smartconnect:1.0\
-"
-
-   set list_ips_missing ""
-   common::send_gid_msg -ssname BD::TCL -id 2011 -severity "INFO" "Checking if the following IPs exist in the project's IP catalog: $list_check_ips ."
-
-   foreach ip_vlnv $list_check_ips {
-      set ip_obj [get_ipdefs -all $ip_vlnv]
-      if { $ip_obj eq "" } {
-         lappend list_ips_missing $ip_vlnv
-      }
-   }
-
-   if { $list_ips_missing ne "" } {
-      catch {common::send_gid_msg -ssname BD::TCL -id 2012 -severity "ERROR" "The following IPs are not found in the IP Catalog:\n  $list_ips_missing\n\nResolution: Please add the repository containing the IP(s) to the project." }
-      set bCheckIPsPassed 0
-   }
-
-}
-
-if { $bCheckIPsPassed != 1 } {
-  common::send_gid_msg -ssname BD::TCL -id 2023 -severity "WARNING" "Will not continue with creation of design due to the error(s) above."
-  return 3
-}
 
 ##################################################################
 # DESIGN PROCs
